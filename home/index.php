079c4b61307803ed909809f37a2cadff6b4466c9
Bommberk <jim.mohncke@rshw.de>
2022-07-13 22:24:25 +0200

    bild

--- home/index.php
+++ home/index.php
@@ -66,6 +66,7 @@ $database = new Medoo([
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="shortcut icon" href="assets/img/bencke_logo.png" type="image/x-icon">
     <title>Bencke</title>
     <link rel="stylesheet" href="../assets/css/style.css">
     <link rel="stylesheet" href="../assets/css/home.css">
